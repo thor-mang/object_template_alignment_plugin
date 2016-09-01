60fb78c083b9740445fa3f273173ae323b8b1ec5
Sebastian Müller <seb.mueller.91@web.de>
2016-09-01 15:52:26 +0200

    added getTemplatePointcloud()

--- src/pointcloud_alignment_server.cpp
+++ src/pointcloud_alignment_server.cpp
@@ -3,6 +3,20 @@
 #include <object_template_alignment_plugin/PointcloudAlignmentAction.h>
 #include <geometry_msgs/PoseStamped.h>
 
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <stdlib.h>
+
+#include "Eigen/Dense"
+#include "Eigen/SVD"
+
+using namespace Eigen;
+using namespace std;
+
+MatrixXd getTemplatePointcloud(string path, string filename);
+
 class PointcloudAlignmentAction
 {
 protected:
@@ -21,10 +35,28 @@ public:
 
 	~PointcloudAlignmentAction(void) {}
   
-    void executeCB(const object_template_alignment_plugin::PointcloudAlignmentGoalConstPtr &goal)
-    {
+    void executeCB(const object_template_alignment_plugin::PointcloudAlignmentGoalConstPtr &goal) {
+		bool success = true;
+		
+		std::string filename = "plug.pcd";
+		std::string path = "vigir/vigir_templates/vigir_template_library/object_library/tools/";
+		
+		MatrixXd template_pointcloud = getTemplatePointcloud(path, filename);
+		
+		cout<<template_pointcloud<<endl;
+		
+		if(success)
+        {
+          geometry_msgs::PoseStamped result;
+          geometry_msgs::Quaternion orientation;
+          result.pose.orientation = orientation;
+          result_.transformation_matrix = result;
+          ROS_INFO("%s: Succeeded", action_name_.c_str());
+          // set the action state to succeeded
+          as_.setSucceeded(result_);
+        }
         // helper variables
-        ros::Rate r(1);
+        /*ros::Rate r(1);
         bool success = true;
 
         // push_back the seeds for the fibonacci sequence
@@ -61,7 +93,7 @@ public:
           ROS_INFO("%s: Succeeded", action_name_.c_str());
           // set the action state to succeeded
           as_.setSucceeded(result_);
-        }
+        }*/
     }
 };
 
@@ -72,4 +104,47 @@ int main(int argc, char** argv) {
     ros::spin();
 
 	return 0;
+}
+
+std::string get_working_path()
+{
+	int MAXPATHLEN = 300;
+	char temp[MAXPATHLEN];
+	return ( getcwd(temp, MAXPATHLEN) ? std::string( temp ) : std::string("") );
+}
+
+MatrixXd getTemplatePointcloud(string path, string filename) {
+	std::ifstream file;
+	string full_filename = path + filename; // TODO: / am Ende von path abfragen
+	// TODO: Endung auf .pcd ueberpruefen
+	
+	cout<<"full filename: "<<full_filename<<endl;
+	
+	cout<<"cur dir: "<<get_working_path()<<endl;
+	
+	file.open(full_filename.c_str());
+    if (!file.is_open()) {
+        std::cerr<<"Error while reading the input file!"<<std::endl;
+        return MatrixXd::Identity(3,3);
+    }
+    
+    string tmp;
+    do {
+		file >> tmp;
+	} while (tmp != "POINTS");
+	
+	int number_points;
+	file >> number_points;
+	
+	file>>tmp;
+	file>>tmp;
+	
+    MatrixXd pointcloud(3,number_points);
+    for (int i = 1; i < number_points; i++) {
+		file >> pointcloud(0,i);
+		file >> pointcloud(1,i);
+		file >> pointcloud(2,i);
+	} 
+	
+	return pointcloud;
 }
