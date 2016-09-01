6e14d5a442b3cc5d1631f73990fc2fdede4dc27c
Alberto Romay <romay@sim.tu-darmstadt.de>
2016-09-01 10:46:04 +0200

    Created simple percentage example

--- src/pointcloud_alignment_server.cpp
+++ src/pointcloud_alignment_server.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include <object_template_alignment_plugin/PointcloudAlignmentAction.h>
+#include <geometry_msgs/PoseStamped.h>
 
 class PointcloudAlignmentAction
 {
@@ -20,17 +21,55 @@ public:
 
 	~PointcloudAlignmentAction(void) {}
   
-	void executeCB(const object_template_alignment_plugin::PointcloudAlignmentGoalConstPtr &goal) {}
+    void executeCB(const object_template_alignment_plugin::PointcloudAlignmentGoalConstPtr &goal)
+    {
+        // helper variables
+        ros::Rate r(1);
+        bool success = true;
+
+        // push_back the seeds for the fibonacci sequence
+        feedback_.percentage = 0;
+
+        // publish info to the console for the user
+        ROS_INFO("%s: Executing aligning template", action_name_.c_str());
+
+        // start executing the action
+        for(int i=1; i<=goal->command; i++)
+        {
+          // check that preempt has not been requested by the client
+          if (as_.isPreemptRequested() || !ros::ok())
+          {
+            ROS_INFO("%s: Preempted", action_name_.c_str());
+            // set the action state to preempted
+            as_.setPreempted();
+            success = false;
+            break;
+          }
+          feedback_.percentage = (float(i)/float(goal->command))*100;
+          // publish the feedback
+          as_.publishFeedback(feedback_);
+          // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+          r.sleep();
+        }
+
+        if(success)
+        {
+          geometry_msgs::PoseStamped result;
+          geometry_msgs::Quaternion orientation;
+          result.pose.orientation = orientation;
+          result_.transformation_matrix = result;
+          ROS_INFO("%s: Succeeded", action_name_.c_str());
+          // set the action state to succeeded
+          as_.setSucceeded(result_);
+        }
+    }
 };
 
 int main(int argc, char** argv) {
-	std::cout<<"blabla"<<std::endl; 
-	//ros::init(argc, argv, "pointcloud alignment");
+    ros::init(argc, argv, "pointcloud_alignment");
 
-	//PointcloudAlignmentAction pointcloud_alignment(ros::this_node::getName());
-	//PointcloudAlignmentAction pointcloud_alignment(ros::this_node::getName());
-	
-	//ros::spin();
+    PointcloudAlignmentAction pointcloud_alignment(ros::this_node::getName());
+    ros::spin();
 
 	return 0;
 }
