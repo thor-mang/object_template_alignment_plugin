b7fcd1bb817adad582cd1efa0d51a5245347e36d
Sebastian Müller <seb.mueller.91@web.de>
2016-09-17 19:50:31 +0200

    added global rotation search and random filter, both not working yet

--- src/pointcloud_alignment_server.cpp
+++ src/pointcloud_alignment_server.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <float.h>
 
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/SVD>
@@ -37,9 +38,29 @@
 using namespace Eigen;
 using namespace std;
 
+typedef struct Cube {
+    VectorXf r0;
+    float half_edge_length;
+    float lower_bound;
+    float upper_bound;
+    int depth;
+} Cube;
+
+typedef struct QueueElement {
+    Cube *cube;
+    struct QueueElement *next;
+} QueueElement;
+
+typedef struct PriorityQueue {
+    QueueElement *head;
+} PriorityQueue;
+
+
 MatrixXf getTemplatePointcloud(string path, string filename);
 void saveDataToFile(VectorXf transformation, MatrixXf rotation, MatrixXf template_cloud, MatrixXf world_cloud);
 
+bool find_pointcloud_alignment(MatrixXf pc1, MatrixXf pc2, float eps, VectorXf t_init, float s_init, MatrixXf &R_icp, VectorXf &t_icp, float &s_icp,
+    float &error, int &itCt, int maxIcpIt, float icpEps, int maxDepth, float inlier_portion, float maxTime);
 int get_inlier_number(float inlier_portion, int number_points);
 float trimmed_scaling_icp(MatrixXf pc1, MatrixXf pc2, MatrixXf &R_result, VectorXf &t_result, float &s_result, MatrixXf R_init,
         VectorXf t_init, float s_init, float eps, int maxIt, float inlier_portion);
@@ -52,11 +73,30 @@ float calc_error(MatrixXf pc1, MatrixXf pc2);
 void sort(VectorXf &v);
 VectorXf matrixToVector(MatrixXf m);
 
+void getEulerAxisRotationMatrix(VectorXf r, MatrixXf &R);
+Cube **splitCube(Cube *cube);
+int depthNumber(PriorityQueue *Q, int depth);
+int *getPermutation(int start, int end);
+Cube **createPriorityQueue(int maxDepth, int &queueLength);
+void fillPriorityQueue(PriorityQueue *Q, Cube *cube, int curDepth, int maxDepth);
+PriorityQueue *createQueue();
+void insert(PriorityQueue *queue, Cube *cube);
+void deleteQueue(PriorityQueue *queue);
+int length(PriorityQueue *queue);
+Cube *extractFirstElement(PriorityQueue *queue);
+float getPassedTime(struct timeval start);
+MatrixXf getAARot(VectorXf r);
+Cube* createCube(VectorXf r0, float half_edge_length, int depth);
+bool betterThan(Cube *cube1, Cube *cube2);
+
+
+
 static int currentTemplateId;
 static VectorXf currentPosition;
 static MatrixXf currentRotation;
 static MatrixXf currentTargetPointcloud;
 static pcl::KdTreeFLANN<pcl::PointXYZ> targetKdTree;
+static bool pointcloudReceived = false, templateReceived = false, templateListReceived = false;
 
 class PointcloudAlignmentAction
 {
@@ -80,6 +120,20 @@ public:
     ~PointcloudAlignmentAction(void) {}
 
     void executeCB(const object_template_alignment_plugin::PointcloudAlignmentGoalConstPtr &goal) {
+        if (pointcloudReceived == false || templateReceived == false || templateListReceived == false) {
+            if (pointcloudReceived == false) {
+                ROS_ERROR("No pointcloud received - Please send a pointcloud request first.");
+            }
+            if (templateReceived == false) {
+                ROS_ERROR("No template received - Please choose template and double click on it.");
+            }
+            if (templateListReceived == false) {
+                ROS_ERROR("No template position available - Please move the template to send position signal.");
+            }
+
+            return;
+        }
+
         bool success = true;
 
         std::string filename = "DRC_drill.pcd";
@@ -87,6 +141,35 @@ public:
         std::string path = "vigir/vigir_templates/vigir_template_library/object_library/tools/";
 
         MatrixXf template_pointcloud = getTemplatePointcloud(path, filename);
+
+
+        /*float discardPointThreshold = 1.5;
+        float maxRadius = FLT_MIN;
+        VectorXf center(3), curPoint(3);
+        center << 0, 0, 0;
+
+        for (int i = 0; i < pc_->size(); i++) {
+            curPoint << pc_->at(i).x, pc_->at(i).y, pc_->at(i).z;
+            center = (center*((float) i) + curPoint)/((float) i+1);
+        }
+
+        for (int i = 0; i < pc_->size; i++) {
+            curPoint << pc_->at(i).x, pc_->at(i).y, pc_->at(i).z;
+            float curRadius = (curPoint-center).norm();
+
+            if (curRadius < maxRadius) {
+                maxRadius = curRadius;
+            }
+        }
+
+        for (int i = pc_->size()-1; i >= 0; i--) {
+            curPoint << pc_->at(i).x, pc_->at(i).y, pc_->at(i).z;
+            float curRadius = (curPoint-center).norm();
+
+            if (curRadius > th)
+        }*/
+
+
 
 
         MatrixXf R_icp(3,3);
@@ -99,7 +182,14 @@ public:
         t_icp = currentPosition;
         s_icp = 1.;
 
-        float error = trimmed_scaling_icp(template_pointcloud, currentTargetPointcloud, R_icp, t_icp, s_icp, currentRotation, currentPosition, 1.0, 1e-3, 100, 0.4);
+        float error;
+        int itCt = 0;
+
+        //bool icp_success = find_pointcloud_alignment(template_pointcloud, currentTargetPointcloud, 1, currentPosition, 1., R_icp, t_icp, s_icp, error,
+        //                                             itCt, 100, 1e-3, 4, 0.4, 10);
+        //cout<<"icp success: "<<icp_success<<", error: "<<error<<endl;
+
+        error = trimmed_scaling_icp(template_pointcloud, currentTargetPointcloud, R_icp, t_icp, s_icp, currentRotation, currentPosition, 1.0, 1e-3, 100, 0.4);
 
         cout<<"error: "<<error<<endl;
 
@@ -108,6 +198,8 @@ public:
         //R_icp = currentRotation;
         //t_icp = currentPosition;
         //s_icp = 1.;
+
+
 
         if (success)
         {
@@ -190,6 +282,7 @@ void templateListCallback(const vigir_object_template_msgs::TemplateServerList::
             2.0f*qx*qz - 2.0f*qy*qw, 2.0f*qy*qz + 2.0f*qx*qw, 1.0f - 2.0f*qx*qx - 2.0f*qy*qy;
     }
 
+    templateListReceived = true;
 }
 
 
@@ -197,9 +290,13 @@ void templateListCallback(const vigir_object_template_msgs::TemplateServerList::
 void templateSelectionCallback(const vigir_ocs_msgs::OCSObjectSelection::ConstPtr& newTemplate) {
     cout<<"I received a new template"<<endl;
     currentTemplateId = newTemplate->id;
+
+    templateReceived = true;
 }
 
 void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& pointcloud) {
+
+
 
     boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ> > pc_ (new pcl::PointCloud<pcl::PointXYZ>());
     pcl::fromROSMsg(*pointcloud, *pc_);
@@ -216,6 +313,14 @@ void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& pointcloud) {
 
 
     targetKdTree.setInputCloud (pc_);
+
+    boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ> > pc2_ (new pcl::PointCloud<pcl::PointXYZ>());
+
+    /*for (int i = 0; i < pc_->size(); i++) {
+        pc2_->insert(i,)
+    }*/
+
+    pointcloudReceived = true;
 }
 
 int main(int argc, char** argv) {
@@ -278,7 +383,6 @@ MatrixXf getTemplatePointcloud(string path, string filename) {
     return pointcloud;
 }
 
-
 // save test data to file to be able to work on icp without having to run the simulation every time
 void saveDataToFile(VectorXf transformation, MatrixXf rotation, MatrixXf template_cloud, MatrixXf world_cloud) {
     ofstream transformationFile, rotationFile, templateFile, worldFile;
@@ -331,7 +435,51 @@ void saveDataToFile(VectorXf transformation, MatrixXf rotation, MatrixXf templat
 // ************************************ ICP algorithm **************************************************
 // *****************************************************************************************************
 
+bool find_pointcloud_alignment(MatrixXf pc1, MatrixXf pc2, float eps, VectorXf t_init, float s_init, MatrixXf &R_icp, VectorXf &t_icp, float &s_icp,
+    float &error, int &itCt, int maxIcpIt, float icpEps, int maxDepth, float inlier_portion, float maxTime) {
 
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
+    int queueLength;
+    Cube **Q = createPriorityQueue(maxDepth, queueLength);
+
+    volatile bool success = false;
+
+    itCt = 0;
+
+    #pragma omp parallel for shared(success, R_icp, t_icp, s_icp)
+    for (int i = 0; i < queueLength; i++) {
+        if (success == true || getPassedTime(start) > maxTime) {
+            continue;
+        }
+
+        MatrixXf R_i(3,3);
+        VectorXf t_i(3);
+        float s_i;
+
+        //int number_inliers = get_inlier_number(inlier_portion, pc1.cols());
+
+        float err = trimmed_scaling_icp(pc1, pc2, R_i, t_i, s_i, getAARot(Q[itCt++]->r0), t_init, s_init, 1e-4, maxIcpIt, inlier_portion);
+        #pragma omp critical
+        {
+            if (err < eps && s_i > 0.8) {
+                error = err;
+
+                R_icp = R_i;
+                t_icp = t_i;
+                s_icp = s_i;
+
+                cout<<"success!!!"<<endl;
+                cout<<R_icp<<endl<<t_icp<<endl<<s_icp<<endl;
+
+                success = true;
+            }
+        }
+    }
+
+    return success;
+}
 
 int get_inlier_number(float inlier_portion, int number_points) {
     return (int) floor(inlier_portion*((float) number_points));
@@ -512,4 +660,254 @@ VectorXf matrixToVector(MatrixXf m) {
     m.transposeInPlace();
     VectorXf v(Map<VectorXf>(m.data(), m.cols()*m.rows()));
     return v;
+}
+
+void getEulerAxisRotationMatrix(VectorXf r, MatrixXf &R) {
+    R = MatrixXf::Identity(3,3);
+    if (r.norm() == 0) {
+        return;
+    }
+
+    MatrixXf r_x(3,3);
+    r_x << 0, -r(2), r(1),
+        r(2), 0, -r(0),
+        -r(1), r(0), 0;
+
+    R += (r_x*sin(r.norm()))/(r.norm());
+    R += (r_x*r_x*(1-cos(r.norm())))/(r.norm()*r.norm());
+}
+
+Cube **splitCube(Cube *cube) {
+    Cube **subcubes = new Cube*[8];
+    VectorXf offset(3);
+    float hel = cube->half_edge_length/2.;
+    float signs[2] = {-1.,+1.};
+
+    int position = 0;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                offset << hel*signs[i],hel*signs[j],hel*signs[k];
+                subcubes[position++] = createCube(cube->r0 + offset, hel, cube->depth + 1);
+            }
+        }
+    }
+
+    return subcubes;
+}
+
+int depthNumber(PriorityQueue *Q, int depth) {
+    int ct = 0;
+    QueueElement *tmp = Q->head;
+    while (tmp != NULL) {
+        if (tmp->cube->depth == depth) {
+            ct++;
+        }
+        tmp = tmp->next;
+    }
+    return ct;
+}
+
+int *getPermutation(int start, int end) {
+    int length = end-start+1;
+
+    vector<int> indices;
+    for (int i = 0; i < length; i++) {
+        indices.push_back(start+i);
+    }
+    random_shuffle(indices.begin(), indices.end());
+
+    int *permutation = new int[length];
+    for (int i = 0; i < length; i++) {
+        permutation[i] = indices[i];
+    }
+
+    return permutation;
+}
+
+Cube **createPriorityQueue(int maxDepth, int &queueLength) {
+    PriorityQueue *Q = createQueue();
+    VectorXf r0_init(3);
+    r0_init<<0,0,0;
+    Cube *C_init = createCube(r0_init, M_PI, 0);
+    insert(Q, C_init);
+    fillPriorityQueue(Q, C_init, 0, maxDepth);
+    int nCubes = length(Q);
+    Cube **priorityQueue = new Cube *[nCubes];
+    int depth = 0;
+    int startPos = 0;
+    while (depth <= maxDepth) {
+        int ct = depthNumber(Q, depth++);
+        int *offset = getPermutation(0, ct-1);
+
+        for (int i = 0; i < ct; i++) {
+            Cube *tmp = extractFirstElement(Q);
+            priorityQueue[startPos+offset[i]] = tmp;
+        }
+        startPos += ct;
+    }
+
+    queueLength = nCubes;
+
+    return priorityQueue;
+}
+
+void fillPriorityQueue(PriorityQueue *Q, Cube *cube, int curDepth, int maxDepth) {
+    float vecLength = sqrt(cube->r0[0]*cube->r0[0] + cube->r0[1]*cube->r0[1] + cube->r0[2]*cube->r0[2]);
+
+    if (vecLength < M_PI) {
+        insert(Q, cube);
+    }
+
+    if (curDepth < maxDepth) {
+        Cube **subcubes = splitCube(cube);
+
+        for (int i = 0; i < 8; i++) {
+            fillPriorityQueue(Q, subcubes[i], curDepth + 1, maxDepth);
+        }
+    }
+}
+
+PriorityQueue *createQueue() {
+    PriorityQueue *queue = new PriorityQueue;
+    queue->head = NULL;
+    return queue;
+}
+
+void insert(PriorityQueue *queue, Cube *cube) {
+    if (queue == NULL) {
+        perror("queue == NULL");
+        return;
+    }
+    QueueElement *newElement = new QueueElement;
+    newElement->cube = cube;
+
+    if (queue->head == NULL) {
+        queue->head = newElement;
+        queue->head->next = NULL;
+        return;
+
+    } else {
+        if (betterThan(cube, queue->head->cube) == true) {
+            newElement->next = queue->head;
+            queue->head = newElement;
+            return;
+        }
+        QueueElement *tmp = queue->head;
+        while (tmp->next != 0) {
+            if (betterThan(cube, tmp->next->cube) == true) {
+                newElement->next = tmp->next;
+                tmp->next = newElement;
+                return;;
+            }
+
+            tmp = tmp->next;
+        }
+        tmp->next = newElement;
+        newElement->next = NULL;
+    }
+}
+
+void deleteQueue(PriorityQueue *queue) {
+    if (queue == NULL)
+        return;
+    if (queue->head == NULL)
+        free(queue);
+    QueueElement *temp = queue->head, *next = NULL;
+    while (temp != NULL) {
+        next = temp->next;
+        delete(temp);
+        temp = next;
+    }
+    delete(queue);
+}
+
+int length(PriorityQueue *queue) {
+    if (queue == NULL || queue->head == NULL)
+        return 0;
+
+    int counter = 0;
+    QueueElement *temp = queue->head;
+    while (temp != NULL) {
+        counter++;
+        temp = temp->next;
+    }
+    return counter;
+}
+
+Cube *extractFirstElement(PriorityQueue *queue) {
+    if (queue == NULL || queue->head == NULL)
+        return NULL;
+
+    QueueElement *element = queue->head;
+    Cube *cube = element->cube;
+    queue->head = queue->head->next;
+    delete(element);
+
+    return cube;
+}
+
+float getPassedTime(struct timeval start) {
+    struct timeval end;
+    gettimeofday(&end, NULL);
+
+    return (float) (((1.0/1000)*((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)))/1000.);
+}
+
+MatrixXf getAARot(VectorXf r) {
+    MatrixXf R = MatrixXf::Identity(3,3);
+
+    if (r.norm() == 0) {
+        return R;
+    }
+
+    MatrixXf r_x(3,3);
+    r_x << 0, -r(2), r(1),
+        r(2), 0, -r(0),
+        -r(1), r(0), 0;
+
+    R += (r_x*sin(r.norm()))/(r.norm());
+    R += (r_x*r_x*(1-cos(r.norm())))/(r.norm()*r.norm());
+
+    return R;
+}
+
+Cube* createCube(VectorXf r0, float half_edge_length, int depth) {
+    Cube *C = new Cube;
+    C->r0 = r0;
+    C->half_edge_length = half_edge_length;
+    C->depth = depth;
+
+    return C;
+}
+
+bool betterThan(Cube *cube1, Cube *cube2) {
+    if (cube1->depth < cube2->depth) {
+        return true;
+    } else if (cube1->depth == cube2->depth && cube1->lower_bound < cube2->lower_bound) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+MatrixXf random_filter(MatrixXf pc, int number_of_points) {
+    //if (number_of_points > pc.)
+
+    int filtered_size = (int) (sampling_pc * ((float) pc.cols()));
+
+    vector<int> indices;
+    for (int i = 0; i < pc.cols(); i++) {
+        indices.push_back(i);
+    }
+    random_shuffle(indices.begin(), indices.end());
+
+    MatrixXf filtered_pc(pc.rows(), filtered_size);
+    for (int i = 0; i < filtered_size; i++) {
+        filtered_pc(0,i) = pc(0, indices[i]);
+        filtered_pc(1,i) = pc(1, indices[i]);
+        filtered_pc(2,i) = pc(2, indices[i]);
+    }
+
+    return filtered_pc;
 }
